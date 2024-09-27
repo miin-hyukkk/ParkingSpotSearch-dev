e7a76269bbe40545ba01885ab48daf501576ff20
miin-hyukkk <ham9893@naver.com>
2024-09-27 12:46:58 +0900

    feat: 반응형 UI 기능 구현

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../../components/main/sidebar";
 import Map from "../../components/main/map";
 
 export default function Main() {
+  const [isMapVisible, setIsMapVisible] = useState(true);
+  const [isApiCalled, setIsApiCalled] = useState(false);
+
+  const handleParkingSelect = () => {
+    if (window.innerWidth < 1024) {
+      setIsMapVisible(true);
+      setIsApiCalled(false);
+    }
+  };
+
   return (
-    <div className="flex flex-row items-center justify-center w-full h-screen text-2xl bg-white">
-      <div className="w-[32%] h-full p-6">
-        <Sidebar />
+    <div className="flex flex-col items-center justify-center w-full h-screen text-2xl bg-white lg:flex-row">
+      <div className="w-full lg:w-[32%] h-screen p-6">
+        <Sidebar
+          onParkingSelect={handleParkingSelect}
+          isApiCalled={isApiCalled}
+          setIsApiCalled={setIsApiCalled}
+        />
       </div>
-      <div className="w-[68%] h-full p-6">
+      <div
+        className={`w-full lg:w-[68%] h-full p-6 ${isMapVisible && !isApiCalled ? "block lg:block" : "hidden lg:block"}`}
+      >
         <Map />
       </div>
     </div>
