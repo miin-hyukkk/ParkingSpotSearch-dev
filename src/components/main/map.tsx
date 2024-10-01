95c26dad0dfa19fcf1f8254e589d722ebd4c99fa
miin-hyukkk <ham9893@naver.com>
2024-10-01 17:22:02 +0900

    feat: 필터링 버튼 UI 구현

--- src/components/main/map.tsx
+++ src/components/main/map.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import useMap from "../../hooks/useMap";
+import useFilter from "../../hooks/useFilter";
 
 export default function Map() {
   const { buttons } = useMap();
+  const { filterButtons } = useFilter();
 
   return (
     <div>
@@ -11,6 +13,26 @@ export default function Map() {
         className="relative h-full"
         style={{ width: "100%", height: "100vh" }}
       >
+        {/* 필터 버튼들 */}
+        <div className="absolute flex flex-row items-center space-x-4 top-8 left-12">
+          {filterButtons.map(button => (
+            <button
+              onClick={button.onClick}
+              type="button"
+              className={button.className}
+              aria-label={button["aria-label"]}
+              style={{ zIndex: 10, width: "auto", height: "35px" }}
+              key={button["aria-label"]}
+            >
+              <i
+                className={`${button.iconClass} mr-2`}
+                style={{ fontSize: "13px" }}
+              />
+              <span className="text-black">{button.label}</span>
+            </button>
+          ))}
+        </div>
+        {/* 줌 제어 및 현재 위치 버튼 */}
         <div className="absolute flex flex-col items-center space-y-4 bottom-20 right-12">
           {buttons.map(button => (
             <button
@@ -19,6 +41,7 @@ export default function Map() {
               className={button.className}
               aria-label={button["aria-label"]}
               style={{ zIndex: 10, width: "60px", height: "60px" }}
+              key={button["aria-label"]}
             />
           ))}
         </div>
