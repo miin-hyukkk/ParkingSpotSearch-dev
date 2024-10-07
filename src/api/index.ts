03cff3c663f73662e090a2d2e4e167965691de55
miin-hyukkk <ham9893@naver.com>
2024-10-07 12:18:10 +0900

    config: 버셀 설정

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,10 +2,9 @@ import axios from "axios";
 import ParkingDataRequest from "../interfaces/parkingDataRequest";
 
 const loadParkingData = async ({ start, end, region }: ParkingDataRequest) => {
-  const apiKey = process.env.REACT_APP_SEOUL_API_KEY;
   try {
     const response = await axios.get(
-      `http://openapi.seoul.go.kr:8088/${apiKey}/json/GetParkingInfo/${start}/${end}/${encodeURIComponent(region)}`,
+      `/api/GetParkingInfo/${start}/${end}/${encodeURIComponent(region)}`,
     );
     return response.data;
   } catch (error) {
@@ -13,4 +12,5 @@ const loadParkingData = async ({ start, end, region }: ParkingDataRequest) => {
     throw error;
   }
 };
+
 export default loadParkingData;
